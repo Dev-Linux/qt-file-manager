10a74ca286c506e18ed2736ccd036bde38886aa6
Silviu Bogan <silviubogan@gmail.com>
2013-07-26 16:13:55 +0300

    Wrapped doxygen comments in main.cpp at 72 chars

--- main.cpp
+++ main.cpp
@@ -4,28 +4,32 @@
 /**
  * @mainpage
  *
- * @todo Subclass @q{core,QFileInfo} and all the classes
- * that use it till you get to your custom FileSystemModel and implement in
- * FileInfo a persistent caching mechanism. d_ptr is protected
- * so you will be able to acces it. The problem with the Qt implementation?
- * @q{core,QFileInfo} fi("A:"); fi.size(); queries the floppy disk and makes a
- * disappointing noise and slows down the application.
- * Windows File Explorer is always open and always monitors the drives so it
- * has an injust advantage which must be compensated by a lot of cool and
- * pleasant work.
- * @note If I see high CPU usage, a possible cause is painting outside the
- * bounding rectangle of an item in GraphView.
+ * @todo Subclass @q{core,QFileInfo} and all the classes that use it
+ * till you get to your custom FileSystemModel and implement in
+ * FileInfo a persistent caching mechanism. d_ptr is protected so you
+ * will be able to acces it. The problem with the Qt implementation?
+ * @q{core,QFileInfo} fi("A:"); fi.size(); queries the floppy disk and
+ * makes a disappointing noise and slows down the application. Windows
+ * File Explorer is always open and always monitors the drives so it
+ * has an injust advantage which must be compensated by a lot of cool
+ * and pleasant work.
+ *
+ * @note If I see high CPU usage, a possible cause is painting outside
+ * the bounding rectangle of an item in GraphView.
  *
  * Documentation notes
  * -------------------
  * - <strong>\@q{module,class}</strong> links to <a
- * href="https://qt-project.org/doc/qt-5.1/">Qt 5.1 reference</a> at the
- * specified class in the specified module. Example: \@q{core,QFileInfo}
- * produces this output: @q{core,QFileInfo}.
+ * href="https://qt-project.org/doc/qt-5.1/">Qt 5.1 reference</a> at
+ * the specified class in the specified module. Example:
+ * \@q{core,QFileInfo} produces this output: @q{core,QFileInfo}.
+ *
  * - Doxygen bug: **\\@** should produce a bold \@.
+ *
  * - Doxygen can't document lambdas (this is not that bad).
- * - When I don't want a reference when typing a symbol, I can use \% before the
- * symbol. This will prevent the creation of a reference.
+ *
+ * - When I don't want a reference when typing a symbol, I can use \%
+ * before the symbol. This will prevent the creation of a reference.
  *
  * Programming tricks
  * ------------------
